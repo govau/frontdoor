46ea548981968efc447674e15a741b84993476d1
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-08-21 11:24:33 +1000

    update port

--- ui/express.js
+++ ui/express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 9001;
+const port = process.env.PORT || 9001;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -9,4 +9,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`ui is listening on port ${port}!`));

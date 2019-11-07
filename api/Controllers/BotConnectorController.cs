bb6ab5c8a9235c743357fda1737a363eea50a0f1
Sam <malmas18@hotmail.com>
2019-11-07 23:38:01 +1100

    add api

---
+++ api/Controllers/BotConnectorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Dta.Frontdoor.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BotConnectorController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public BotConnectorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"BotConnector {_configuration["botConnector"]}");
+                var response = await client.GetStringAsync("https://webchat.botframework.com/api/tokens");
+                return response;
+            }
+        }
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Dta.Frontdoor.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var vcapServicesString = Environment.GetEnvironmentVariable("VCAP_SERVICES");
+            Console.WriteLine(vcapServicesString);
+            if (!string.IsNullOrEmpty(vcapServicesString))
+            {
+                dynamic vcapServices = JsonConvert.DeserializeObject<IDictionary<string, dynamic>>(vcapServicesString);
+                foreach (JObject ups in vcapServices["user-provided"])
+                {
+                    var credentials = JObject.FromObject(ups["credentials"]);
+                    foreach (var credentialProps in credentials.Properties())
+                    {
+                        Environment.SetEnvironmentVariable(credentialProps.Name, credentialProps.Values().SingleOrDefault().Value<string>());
+                    }
+                }
+            }
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hc, c) =>
+                {
+                    c.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        c.AddCommandLine(args);
+                    }
+                })
+                .UseStartup<Startup>();
+    }
+}

3333e192fd72defee7ab402e9f31b05c867661eb
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-11-13 10:38:04 +1100

    update express config

--- ui/express.js
+++ ui/express.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 9001;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 

a835f469aa9f39b1ed049ceac524573072e6a5a6
Sam <malmas18@hotmail.com>
2019-11-22 08:44:34 +1100

    split gatsby components

--- ui/src/components/NavigationBar.tsx
+++ ui/src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
-import AUmainNav, { AUmainNavContent } from '@gov.au/main-nav';
-// import { Link } from 'gatsby';
+//import AUmainNav, { AUmainNavContent } from '@gov.au/main-nav';
+import { Link } from 'gatsby';
 import * as React from 'react';
 
 
@@ -8,37 +8,89 @@ interface INavigationBarProps {
 }
 
 const NavigationBar: React.FC<INavigationBarProps> = () => (
-  <AUmainNav dark>
+  <nav className="au-main-nav" aria-label="main">
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <AUmainNavContent items={[
-            {
-              active: 'true',
-              link: '/',
-              text: 'Home',
-            },
-            {
-              link: '/buyer',
-              text: 'Buyer',
-            },
-            {
-              link: '/seller',
-              text: 'Sellers',
-            },
-            {
-              link: '/news',
-              text: 'News',
-            },
-            {
-              link: '/events',
-              text: 'Events',
-            },
-          ]} />
+          <div id="main-nav-default" className="au-main-nav__content">
+            <button
+              aria-controls="main-nav-default"
+              aria-expanded="false"
+              className="au-main-nav__toggle au-main-nav__toggle--open"
+              onClick="return AU.mainNav.Toggle( this )">
+                Menu
+            </button>
+            <div className="au-main-nav__menu">
+              <div className="au-main-nav__menu-inner">
+                <div className="au-main-nav__focus-trap-top"></div>
+                <button
+                  aria-controls="main-nav-default"
+                  className="au-main-nav__toggle au-main-nav__toggle--close"
+                  onClick="return AU.mainNav.Toggle( this )">
+                    Close
+                </button>
+                <ul className="au-link-list">
+                  <li>
+                    <a href="#">About</a>
+                    <Link to="/" activeClassName="active">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/buyer" activeClassName="active">Buyer</Link>
+                  </li>
+                  <li>
+                    <Link to="/seller" activeClassName="active">Seller</Link>
+                  </li>
+                  <li>
+                    <Link to="/news" activeClassName="active">News</Link>
+                  </li>
+                  <li>
+                    <Link to="/Events" activeClassName="active">events</Link>
+                  </li>
+                </ul>
+                <div className="au-main-nav__focus-trap-bottom"></div>
+              </div>
+            </div>
+            <div
+              className="au-main-nav__overlay"
+              aria-controls="main-nav-default"
+              onClick="return AU.mainNav.Toggle( this )">
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </AUmainNav>
+  </nav>
+  // <AUmainNav dark>
+  //   <div className="container">
+  //     <div className="row">
+  //       <div className="col-md-12">
+  //         <AUmainNavContent items={[
+  //           {
+  //             active: 'true',
+  //             link: '/',
+  //             text: 'Home',
+  //           },
+  //           {
+  //             link: '/buyer',
+  //             text: 'Buyer',
+  //           },
+  //           {
+  //             link: '/seller',
+  //             text: 'Sellers',
+  //           },
+  //           {
+  //             link: '/news',
+  //             text: 'News',
+  //           },
+  //           {
+  //             link: '/events',
+  //             text: 'Events',
+  //           },
+  //         ]} />
+  //       </div>
+  //     </div>
+  //   </div>
+  // </AUmainNav>
 );
 
 export default NavigationBar;
--- ui/src/pages/buyer.tsx
+++ ui/src/pages/buyer.tsx
@@ -1,98 +1,12 @@
-import axios from 'axios';
-// import { DirectLine } from 'botframework-directlinejs';
-// import ReactWebChat from 'botframework-webchat';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React from 'react';
+import BuyerChat from '../components/BuyerChat';
 import DefaultLayout from '../layouts/defaultLayout';
 
 const BuyerPage: React.FC = () => {
-  const [loaded, setLoaded] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [qnas] = useState<any>([]);
-  const inputEl = useRef(null);
-  // const answers = useRef<any>([]);
-  // const directLine = useRef<DirectLine | null>(null);
-
-  const askCallback = useCallback((text: string, question: any) => {
-    setLoading(true);
-
-    qnas.push({
-      question: text,
-    });
-    return axios.post('/api/answer', question)
-      .then((r: any) => {
-        qnas.push(r.data);
-        setLoading(false);
-        return r;
-      });
-  }, [qnas]);
-
-  useEffect(() => {
-    if (!loaded) {
-      const q = 'What level of government do you work for?';
-      askCallback(q, {
-        question: q,
-      });
-      setLoaded(true);
-    }
-  }, [askCallback, loaded]);
-
   return (
     <DefaultLayout>
       {'Buying digital products & services'}
-      <div role="search" aria-label="sitewide" className="au-search">
-        <label htmlFor="standard" className="au-search__label">Search this website</label>
-        <input type="search" id="standard" name="standard" className="au-text-input" ref={inputEl} />
-        <div className="au-search__btn">
-          <button className="au-btn" onClick={() => {
-            if (inputEl.current) {
-              const search: any = inputEl.current;
-              if (search) {
-                askCallback(search.value, {
-                  question: search.value,
-                });
-              }
-            }
-          }}>
-            <span className="au-search__submit-btn-text">
-              Search
-                </span>
-          </button>
-        </div>
-      </div>
-      <div>
-        {loading && 'Searching...'}
-        {qnas && qnas.map((qna: any) => (
-          <>
-            {qna.question && (
-              <div className="row">
-                <div className="col-md-7 margin-bottom-1 background-light-grey">
-                  {qna.question}
-                </div>
-              </div>
-            )}
-            {qna.answers && qna.answers.map((a: any) => (
-              <div className="row">
-                <div className="col-md-push-5 col-md-7 margin-bottom-1 background-blue text-colour-white">
-                  <div>{a.answer}</div>
-                  {a.context && (
-                    <div>
-                      {a.context.prompts && a.context.prompts.map((p: any) => (
-                        <button className="au-btn" data-id={p.qnaId} data-displaytext={p.displayText} onClick={(e: any) => {
-                          askCallback(e.target.dataset.displaytext, {
-                            qnaId: e.target.dataset.id,
-                          });
-                        }}>
-                          {p.displayText}
-                        </button>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
-            ))}
-          </>
-        ))}
-      </div>
+      <BuyerChat />
     </DefaultLayout>
   );
 };

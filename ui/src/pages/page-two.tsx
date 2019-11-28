a8ac30e0df1c333619072371a23f0734acd3daa8
Sam <29763502+malmas18@users.noreply.github.com>
2019-11-28 15:27:29 +1100

    add video

--- ui/src/pages/page-two.tsx
+++ ui/src/pages/page-two.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'gatsby';
 import * as React from 'react';
+import ConceptVideo from './dta_concept.mp4';
 
 import DefaultLayout from '../layouts/defaultLayout';
 
@@ -7,6 +8,9 @@ const PageTwo = () => (
   <DefaultLayout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <video width="640" height="480" controls>
+      <source src={ConceptVideo} type="video/mp4" />
+    </video>
     <ul>
       <li>
         <Link to="/a-markdown-page/">Show me some Markdown!</Link>

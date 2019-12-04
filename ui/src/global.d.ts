901fe7a963805ee8a622f77bfe107a1abe956021
Sam <29763502+malmas18@users.noreply.github.com>
2019-12-04 15:14:31 +1100

    test video

--- ui/src/global.d.ts
+++ ui/src/global.d.ts
@@ -2,3 +2,8 @@ declare module '@gov.au/header'
 declare module '@gov.au/footer'
 declare module '@gov.au/main-nav'
 declare module '@gov.au/searchbox'
+
+declare module '*.mp4' {
+    const videoFiles: any;
+    export default videoFiles;
+}

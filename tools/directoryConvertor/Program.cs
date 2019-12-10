7b5dae41e4023f8609cad816eaa09e0baf974e95
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-12-10 22:26:24 +1100

    add agencies page

---
+++ tools/directoryConvertor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+using System.Text;
+
+namespace Dta.Frontdoor.Tools.DirectoryConvertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "data/directory.gov.au.xml";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var directoryGovAuFilepath = Path.Combine(currentDirectory, filename);
+
+            XElement directoryGovAu = XElement.Load(directoryGovAuFilepath);
+
+            var agencies = directoryGovAu.Descendants("item")
+                                         .Where(item => item != null && item.Element("type_of_body") != null && ((string)item.Element("type_of_body")).Contains("Corporate Commonwealth Entity"))
+                                         .Select(i => new
+                                         {
+                                             title = (string)i.Element("title"),
+                                             typeOfBody = (string)i.Element("type_of_body"),
+                                         })
+                                         .OrderBy(i => i.title)
+                                         .GroupBy(i => i.typeOfBody)
+                                         ;
+
+
+            var sb = new StringBuilder();
+            sb.AppendLine(@"---
+layout: page
+title: 'Agencies'
+---
+            ");
+            foreach (var group in agencies)
+            {
+                sb.AppendLine($"#{group.Key}");
+                Console.WriteLine(group.Key);
+                foreach (var i in group) {   
+                    sb.AppendLine($"* {i.title}");
+                    Console.WriteLine("* " + i.title);
+                }
+                sb.AppendLine("");
+            }
+
+            File.WriteAllText(Path.Combine("data", "agencies.md"), sb.ToString());
+        }
+    }
+}

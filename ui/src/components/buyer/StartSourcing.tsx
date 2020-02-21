42e612828c315b884dacf44e34fa6611568ed3f8
Sam <29763502+malmas18@users.noreply.github.com>
2020-02-21 11:00:01 +1100

    make sourcing stack

--- ui/src/components/buyer/StartSourcing.tsx
+++ ui/src/components/buyer/StartSourcing.tsx
@@ -11,9 +11,13 @@ const StartSourcing: React.FC = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col-sm-12">
+        <div className="col-xs-12 col-sm-2">
           <div className="float-left"><Link to="/buyer/products-and-services/digital-marketplace/" className="margin-sm-right-2 margin-md-right-2">Digital Marketplace</Link></div>
+        </div>
+        <div className="col-xs-12 col-sm-3">
           <div className="float-left"><Link to="#" className="margin-sm-right-2 margin-md-right-2">[TODO]ICT Procurement Portal</Link></div>
+        </div>
+        <div className="col-xs-12 col-sm-3">
           <div className="float-left"><Link to="/buyer/contact-us">List of DTA panels and portals</Link></div>
         </div>
       </div>

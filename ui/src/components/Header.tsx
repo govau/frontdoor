72175df3f9d9beee775af16cbd12c2cfdcd11d07
Sam <29763502+malmas18@users.noreply.github.com>
2020-02-21 14:42:11 +1100

    remove GA

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -1,33 +1,28 @@
 import { Link } from 'gatsby';
 import * as React from 'react';
-import GoogleAnalytics from './GoogleAnalytics';
-
 
 interface IHeaderProps {
   title: string;
 }
 
 const Header: React.FC<IHeaderProps> = () => (
-  <>
-    <GoogleAnalytics />
-    <header className="au-header au-header--dark" role="banner">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <Link className="au-header__brand" to="/">
-              <img className="au-header__brand-image" alt="The Australian Government coat of Arms" src="/header-logo-agov.png" />
-              <div className="au-header__text">
-                <h1 className="au-header__heading">Digital Sourcing for Government</h1>
-                <div className="au-header__subline">
-                  Initiative of the DTA
-                </div>
+  <header className="au-header au-header--dark" role="banner">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <Link className="au-header__brand" to="/">
+            <img className="au-header__brand-image" alt="The Australian Government coat of Arms" src="/header-logo-agov.png" />
+            <div className="au-header__text">
+              <h1 className="au-header__heading">Digital Sourcing for Government</h1>
+              <div className="au-header__subline">
+                Initiative of the DTA
               </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
         </div>
       </div>
-    </header>
-  </>
+    </div>
+  </header>
 );
 
 export default Header;

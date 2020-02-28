0a172abb120cc6fd564c1568a2e0abeae4eddc6a
Sam <29763502+malmas18@users.noreply.github.com>
2020-02-28 15:28:08 +1100

    linting

--- ui/src/components/buyer/Search.tsx
+++ ui/src/components/buyer/Search.tsx
@@ -201,7 +201,7 @@ const Search: React.FC = () => {
           {selectedAgencyType === 'state' && (
             <div className="row margin-sm-top-2 margin-md-top-2">
               <div className="col-sm-8 col-sm-push-2 text-align-center">
-                
+
                   You work for a state, territory or local organisation. This includes educational instututions.
               </div>
             </div>

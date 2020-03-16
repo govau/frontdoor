9f4940c3a88ee83019cce1915bf2e22add37e186
Sam <29763502+malmas18@users.noreply.github.com>
2020-03-16 14:07:45 +1100

    formatting

--- api/Program.cs
+++ api/Program.cs
@@ -1,31 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Dta.Frontdoor.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace Dta.Frontdoor.Api {
+    public class Program {
+        public static void Main(string[] args) {
             var vcapServicesString = Environment.GetEnvironmentVariable("VCAP_SERVICES");
-            if (!string.IsNullOrEmpty(vcapServicesString))
-            {
+            if (!string.IsNullOrEmpty(vcapServicesString)) {
                 dynamic vcapServices = JsonConvert.DeserializeObject<IDictionary<string, dynamic>>(vcapServicesString);
-                foreach (JObject ups in vcapServices["user-provided"])
-                {
+                foreach (JObject ups in vcapServices["user-provided"]) {
                     var credentials = JObject.FromObject(ups["credentials"]);
-                    foreach (var credentialProps in credentials.Properties())
-                    {
+                    foreach (var credentialProps in credentials.Properties()) {
                         Environment.SetEnvironmentVariable(credentialProps.Name, credentialProps.Values().SingleOrDefault().Value<string>());
                     }
                 }
@@ -35,16 +25,14 @@ namespace Dta.Frontdoor.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) {
             var builder = WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hc, c) =>
-                {
+                .ConfigureAppConfiguration((hc, c) => {
                     c.AddEnvironmentVariables();
-                    if (args != null)
-                    {
+                    if (args != null) {
                         c.AddCommandLine(args);
                     }
                 })
                 .UseStartup<Startup>();
-            
+
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PORT"))) {
                 builder = builder.UseUrls($"http://*:{Environment.GetEnvironmentVariable("PORT")}");
             }
--- api/Startup.cs
+++ api/Startup.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Hosting;
 
-namespace Dta.Frontdoor.Api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Dta.Frontdoor.Api {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
         private readonly string _devOrigins = "_devOrigins";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddCors(options => {
-            options.AddPolicy(_devOrigins, builder => {
+                options.AddPolicy(_devOrigins, builder => {
                     builder.WithOrigins("http://localhost:8000")
                         .AllowAnyHeader()
                         .AllowAnyMethod();
@@ -35,19 +23,13 @@ namespace Dta.Frontdoor.Api
             });
             services.AddRazorPages();
             services.AddOptions();
-            
-            // services.AddConfiguration(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+            } else {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseHttpsRedirection();
